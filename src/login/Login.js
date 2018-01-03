f975980d914d1144bcdf40aa422c3dc97a1cae95
CharzaroSmallLaptop <daball86@gmail.com>
2018-01-03 14:30:39 -0500

    Login screens and system

---
+++ src/login/Login.js
@@ -0,0 +1,250 @@
+import React from 'react';
+
+import './Login.css'
+
+class Login extends React.Component{
+
+	constructor(){
+		super();
+		this.state = {
+			signingUp: false,
+			loginError: {
+				visible: false,
+				text: ''
+			}
+		}
+
+		this.usernameTaken = false;
+		this.passwordsDontMatch = false;
+	}
+
+	login(e){
+		var usernameAttempt = this.usernameInput.value;
+		var passwordAttempt = this.passwordInput.value;
+
+		if(!usernameAttempt){
+			var loginError = {
+				visible: true,
+				text: "Please enter a username"
+			}
+			this.updateLoginError(loginError);
+			return;
+		}
+		else if(!passwordAttempt){
+			var loginError = {
+				visible: true,
+				text: "Please enter a password"
+			}
+			this.updateLoginError(loginError);
+			return;
+		}
+		else{
+			console.log("User: " + usernameAttempt);
+			console.log("Password: " + passwordAttempt)
+
+			fetch('/login', {
+				method: 'POST',
+				credentials: 'same-origin',
+				body: JSON.stringify({
+					username: usernameAttempt,
+					password: passwordAttempt,
+					stayLoggedIn: this.checkbox.checked
+				})
+			}).then((res) => {
+				if(res.status == 200){
+					return res.json();
+				}
+				else{
+					console.log("Could not connect to server.\n" + res.statusText);
+					var loginError = {
+						visible: true,
+						text: "Could not connect to server.\n" + res.statusText
+					}
+					this.updateLoginError(loginError);
+					return null;
+				}
+			}).then((result) => {
+				if(result.successful){
+					console.log("SUCCESS, sessionID = " + result.sessionID);
+					this.props.receiveLogin(usernameAttempt, result.sessionID, this.checkbox.checked);
+				}
+				else{
+					console.log("LOGIN FAILED");
+					var loginError = {
+						visible: true,
+						text: "Incorrect username or password"
+					}
+					this.updateLoginError(loginError);
+				}
+			})
+		}
+	}
+
+	newuser(e){
+		var usernameAttempt = this.usernameInput.value;
+		var passwordAttempt = this.passwordInput.value;
+		var confirmPass = this.confirmpasswordInput.value;
+
+		if(!usernameAttempt){
+			var loginError = {
+				visible: true,
+				text: "Please enter a username"
+			}
+			this.updateLoginError(loginError);
+			return;
+		}
+		else if(!passwordAttempt){
+			var loginError = {
+				visible: true,
+				text: "Please enter a password"
+			}
+			this.updateLoginError(loginError);
+			return;
+		}
+		else if(!confirmPass){
+			var loginError = {
+				visible: true,
+				text: "Please confirm your password"
+			}
+			this.updateLoginError(loginError);
+			return;
+		}
+		else{
+			console.log("User: " + usernameAttempt);
+			console.log("Password: " + passwordAttempt)
+
+			if(passwordAttempt != confirmPass){
+				console.log("Passwords do not match");
+				var loginError = {
+					visible: true,
+					text: "Passwords do not match."
+				}
+				this.updateLoginError(loginError);
+				return;
+			}
+
+			fetch('/newuser', {
+				method: 'POST',
+				credentials: 'same-origin',
+				body: JSON.stringify({
+					username: usernameAttempt,
+					password: passwordAttempt
+				})
+			}).then((res) => {
+				if(res.status == 200){
+					return res.json();
+				}
+				else{
+					console.log("Could not connect to server.\n" + res.statusText);
+					var loginError = {
+						visible: true,
+						text: "Could not connect to server.\n" + res.statusText
+					}
+					this.updateLoginError(loginError);
+					return null;
+				}
+			}).then((result) => {
+				if(result.userAlreadyExists){
+					console.log("USERNAME TAKEN");
+					var loginError = {
+						visible: true,
+						text: "Username already taken."
+					}
+					this.updateLoginError(loginError);
+				}
+				else{
+					this.props.receiveLogin(usernameAttempt, result.sessionID, false);
+					console.log("USER ADDED")
+				}
+			})
+		}
+	}
+
+	linkClick(e, signUp){
+		e.preventDefault();
+		this.setState((prevstate) => {
+			return {
+				signingUp: signUp,
+				loginError: prevstate.loginError
+			}
+		})
+	}
+
+	updateLoginError(newerror){
+		this.setState((prevstate) => {
+			return {
+				signingUp: prevstate.signingUp,
+				loginError: newerror
+			}
+		})
+	}
+
+
+	renderPage(signingUp){
+		if(signingUp){
+			return(
+				<div className="loginPanel">
+					<h1 className="loginTitle"> Create a new account </h1>
+					<div className="loginError" style={{
+						diplay: this.state.loginError.visible? 'block': 'none',
+					}}>
+						{this.state.loginError.text}
+					</div>
+					<div className="contentPanel">
+						<form>
+							<label>Username:</label><br />
+							<input className="loginInput" type="text" name="username" ref={(input) => this.usernameInput = input} /><br />
+							<label>Password:</label><br />
+							<input className="loginInput" type="password" name="password" ref={(input) => this.passwordInput = input} /><br />
+							<label>Confirm password:</label><br />
+							<input className="loginInput" type="password" name="confirmpassword" ref={(input) => this.confirmpasswordInput = input} /><br />
+							<button className="loginButton" type="button" onClick={(e) => this.newuser(e)}>Create account</button>
+						</form>
+						<br />
+						<span className="bottomSpan"> Already have an account? <a className="bottomLink" href="" onClick={(e) => this.linkClick(e, false)}>Log in!</a></span>
+					</div>
+				</div>
+			)
+		}
+		else{
+			return (
+				<div className="loginPanel">
+					<h1 className="loginTitle"> Log in </h1>
+					<div className="loginError" style={{
+						diplay: this.state.loginError.visible? 'block': 'none',
+					}}>
+						{this.state.loginError.text}
+					</div>
+					<div className="contentPanel">
+						<form>
+							<label>Username:</label><br />
+							<input className="loginInput" type="text" name="username" ref={(input) => this.usernameInput = input} /><br />
+							<label>Password:</label><br />
+							<input className="loginInput" type="password" name="password" ref={(input) => this.passwordInput = input} /><br />
+							<input className="checkbox" type="checkbox" ref={(input) => this.checkbox = input} />Stay logged in
+							<br />
+							<button className="loginButton" type="button" onClick={(e) => this.login(e)}>Log in</button>
+						</form>
+						<br />
+						
+						<span className="bottomSpan">New to ENotes?<br />
+						<a className="bottomLink" href='' onClick={(e) => this.linkClick(e, true)}> Create a new account!</a></span>
+					</div>
+				</div>
+			)
+		}
+	}
+
+	render(){
+		return (
+			<div>
+				<h1 className="title">Welcome to ENotes!</h1>
+				<div className="loginScreen">
+						{this.renderPage(this.state.signingUp)}
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Login;
---
+++ src/note-page/NotePage.js
@@ -0,0 +1,449 @@
+import React from 'react';
+import Note from '../note/note';
+import note from '../note/note-class';
+import RightClickMenu from '../right-click-menu/rightClickMenu';
+import './NotePage.css';
+
+class NotePage extends React.Component {
+
+  constructor(){
+    super();
+
+    // bind functions for each Note component
+    this.addNote = this.addNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+    this.updateNote = this.updateNote.bind(this);
+    this.selectNote = this.selectNote.bind(this);
+    this.dragStart = this.dragStart.bind(this);
+    this.resizeStart = this.resizeStart.bind(this);
+    this.markUnsaved = this.markUnsaved.bind(this);
+    this.launchOptions = this.launchOptions.bind(this);
+
+    // watch the state of each note
+    this.state = {
+      notes: {},
+      rightClickMenu:  {note: '', display: 'none', x: 0, y: 0}
+
+    }
+
+    this.unsaved = false;
+
+    // object to hold info about the dragging note
+    this.drag = {};
+    this.resize = {};
+
+  }
+
+  // STATE MANAGEMENT FUNCTIONS
+  setNoteState(notes){
+    this.setState((prevstate) => {
+      return {
+        notes: notes,
+        rightClickMenu: prevstate.rightClickMenu
+      }
+    })
+  }
+
+  setRightMenuState(obj){
+    this.setState((prevstate) => {
+      return {
+        notes: prevstate.notes,
+        rightClickMenu: obj
+      }
+    })
+  }
+
+  // LIFE CYCLE FUNCTIONS
+
+  componentWillMount(){
+
+      window.addEventListener('mousedown', (event) => {
+        if(this.state.rightClickMenu.note){
+            console.log("HI")
+            var obj = {
+              note: '',
+              display: 'none',
+              x: 0,
+              y: 0
+            }
+            this.setRightMenuState(obj)
+          }
+      })
+      window.addEventListener('mouseup', (event) => {
+          this.dragEnd(event)
+          this.resizeEnd(event)
+
+      }); // listen for drag ends
+      window.addEventListener('mousemove', (event) => {
+          this.dragDuring(event)
+          this.resizeDuring(event)
+      }); // listen for drag movements
+
+    
+
+    // load notes from DB
+    this.getNotes();
+  }
+
+  componentDidMount(){
+    window.setInterval(() => {
+      if(this.unsaved){
+        this.unsaved = false;
+        var notes = {...this.state.notes}
+        Object.keys(this.state.notes).map((t) =>{
+          if(!notes[t].saved){
+            this.updateNote(t);
+          }
+          return;
+        })
+
+      }
+      
+    }, 1000)
+  }
+
+  // DATA MANAGEMENT FUNCTIONS
+
+  // ask server for notes
+  getNotes(){
+
+    // fetch from /api
+      //* FETCH
+    fetch("/api?sessionID=" + this.props.sessionID)
+    .then((res) => {
+        if(res.status !== 200){
+          console.log(res.statusText);
+        }
+        else{ return res.json()}
+      }) // turn result to json (??)
+        .then((data) => {
+          if(data.sessionExpired){
+            alert("Session Expired");
+            this.logout();
+          }
+          else{
+            this.loadNotes(data.notes); // load note
+            // if no notes were loaded, create a new one
+            if (Object.keys(this.state.notes).length <= 0) {
+                this.addNote(100, 100);
+            }
+          }
+        });
+      //FETCH */
+
+      /* FETCH REMOVE
+      if (Object.keys(this.state.notes).length <= 0) {
+          this.addNote(300, 300);
+      }
+      */
+  }
+
+  // load notes from an array of objects
+  loadNotes(data){
+
+    // start building new notes object for the state
+    var notes = {}
+
+    // add a new note class for each object
+    data.map((anote) => {
+      // build note class
+      var n = new note(anote.content, anote.x, anote.y, anote.width, anote.height);
+      n.zindex = anote.zindex;
+      // insert to notes
+      notes[anote.tag] = n;
+      return;
+    })
+    
+    // use notes as state
+    this.setNoteState(notes);
+  }
+
+  // Adds a new note
+  addNote(x, y) {
+    // build empty note
+    var n = new note('', x, y, 300, 200);
+    n.selected = true; //start selected
+    n.zindex = 9999; // start on top
+
+    // unselect all other notes
+    var notes = {...this.state.notes}
+    Object.keys(notes).map((k) => {
+      notes[k].selected = false;
+      if(notes[k].zindex > 0){
+          notes[k].zindex--;
+      }
+      return;
+    })
+
+    // add to state with time since epoch as a tag
+    var t = `note-${Date.now()}`
+    notes[t] = n;
+    this.setNoteState(notes);
+
+      //* FETCH
+    // update DB
+    fetch("/api", {
+        method: "POST",
+        body: JSON.stringify({
+            sessionID: this.props.sessionID,
+            tag: t,
+            content: n.content,
+            x: n.x,
+            y: n.y,
+            width: n.width,
+            height: n.height,
+            zindex: n.zindex
+        })
+    }).then((result) => {
+      if(result.status !== 200){
+        console.log("ERROR: Server response: " + result.status)
+        console.log(result.statusText);
+      }
+      else{
+        return result.json();
+      }
+      return;
+    }).then((res) =>{
+      if(res.sessionExpired){
+          alert("Session Expired");
+          this.logout();
+      }
+    });
+
+    //FETCH */
+    
+  }
+
+  // delete a note
+  deleteNote(tag){
+    var notes = {...this.state.notes}
+    notes[tag] = null;
+    delete(notes[tag]);
+    this.setNoteState(notes);
+
+    // update DB
+      //* FETCH
+    fetch("/api", {
+        method: "DELETE",
+        body: JSON.stringify({
+          sessionID: this.props.sessionID,
+          tag: tag
+        })
+    }).then((result) => {
+      if(result.status !== 200){
+        console.log("ERROR: Server response: " + result.status)
+        console.log(result.statusText);
+      }
+      else{
+        return result.json()
+      }
+    }).then((res) =>{
+      if(res.sessionExpired){
+          alert("Session Expired");
+          this.logout();
+      }
+    });
+    //FETCH */
+  }
+
+  updateNote(tag) {
+    var notes = { ...this.state.notes };
+    var note = notes[tag];
+
+    console.log("UPDATING")
+    console.log(this.unsaved)
+
+    // update DB
+      //* FETCH
+    fetch("/api", {
+        method: "PUT",
+        body: JSON.stringify({
+          sessionID: this.props.sessionID,
+          tag: tag,
+          newcontent: note.content,
+          newx: note.x,
+          newy: note.y,
+          newW: note.width,
+          newH: note.height,
+          newZ: note.zindex
+        })
+    }).then((result) => {
+      if(result.status !== 200){
+        console.log("ERROR: Server response: " + result.status)
+        console.log(result.statusText);
+      }
+      else{
+        return result.json();
+      }
+    }).then((res) =>{
+      if(res.sessionExpired){
+          alert("Session Expired");
+          this.logout();
+      }
+      else{
+        note.saved = true;
+      }
+    });
+     // FETCH */
+  }
+
+  // Select a note
+  selectNote(tag){
+    // if already selected, done
+    if(this.state.notes[tag].selected){
+      return;
+    }
+
+    // go through each note, deselecting it and moving it back 1
+    // or if it is the note to select, select it and bring it to front
+    var notes = {...this.state.notes};
+    Object.keys(notes).map((k) => {
+      if(k === tag){
+        notes[k].selected = true;
+        notes[k].zindex = 9999;
+      }
+      else{
+        notes[k].selected = false;
+        if(notes[k].zindex > 0){
+          notes[k].zindex--;
+        }
+        
+        }
+      return;
+    })
+    // update state
+    this.setNoteState(notes);
+  }
+
+  // start dragging a note
+  dragStart(tag, x, y){
+    // save the offset from the corner of the note, so when dragging the note will move with the cursor
+    var note = this.state.notes[tag];
+    this.drag = { tag: tag, offsetX: x - note.x, offsetY: y - note.y}
+  }
+
+
+  // Move a note to the cursor when the cursor is moved and a note is being dragged
+  dragDuring(e){
+    // quit now if nothing is being dragged
+    if(!this.drag.tag){
+      return;
+    }
+    // grab the note
+    const notes = {...this.state.notes}
+    var note = notes[this.drag.tag];
+
+    // move note to the cursor minue the offset
+    note.x = e.screenX - this.drag.offsetX;
+    note.y = e.screenY - this.drag.offsetY;
+    // update state
+    this.setNoteState(notes);
+  }
+
+  // stops a dragging note
+  dragEnd(e){
+    if(!this.drag.tag){
+      return;
+    }
+
+    this.updateNote(this.drag.tag)
+    this.drag = {};
+  }
+
+  resizeStart(tag, x, y) {
+      // save the offset from the corner of the note, so when dragging the note will move with the cursor
+      var note = this.state.notes[tag];
+      this.resize = { tag: tag, startX: x, startY: y }
+  }
+
+  resizeDuring(e) {
+    // quit now if nothing is being dragged
+    if (!this.resize.tag) {
+        return;
+    }
+    // grab the note
+    const notes = { ...this.state.notes }
+    var note = notes[this.resize.tag];
+
+    // move note to the cursor minue the offset
+    note.width += e.screenX - this.resize.startX;
+    this.resize.startX = e.screenX;
+    note.height += e.screenY - this.resize.startY;
+    this.resize.startY = e.screenY;
+    // update state
+    this.setNoteState(notes);
+  }
+
+  resizeEnd(e) {
+    if (!this.resize.tag) {
+        return;
+    }
+
+    this.updateNote(this.resize.tag)
+    this.resize = {};
+  }
+
+  markUnsaved(){
+    this.unsaved = true;
+  }
+
+  launchOptions(tag, x, y){
+    var obj = this.state.rightClickMenu;
+
+    obj.note = tag;
+    obj.display = 'block';
+    obj.x = x;
+    obj.y = y;
+
+    this.setRightMenuState(obj);
+  }
+
+  logout(){
+    fetch("/logout", {
+        method: "POST",
+        body: JSON.stringify({
+          sessionID: this.props.sessionID
+        })
+    }).then((result) => {
+      if(result.status !== 200){
+        console.log("ERROR: Server response: " + result.status)
+        console.log(result.statusText);
+      }
+      else{
+        this.props.logout();
+      }
+    });
+  }
+
+  // draws the App
+  render() {
+    return (
+      <div className="App" >
+        <h1 className="title">Welcome {this.props.username}!</h1>
+        <button className="logoutButton" onClick={(e) => this.logout()}>Logout</button>
+        { Object.keys(this.state.notes).map((key) =>
+          <Note
+            key={key}
+            tag={key}
+            note={this.state.notes[key]}
+            addNote={this.addNote}
+            deleteNote={this.deleteNote}
+            updateNote={this.updateNote}
+            selectNote={this.selectNote}
+            dragStart={this.dragStart}
+            resizeStart={this.resizeStart}
+            markUnsaved={this.markUnsaved}
+
+            launchOptions={this.launchOptions}
+          />  
+        )
+        }
+        <RightClickMenu 
+          obj={this.state.rightClickMenu}
+        />
+      </div>
+    );
+  }
+}
+
+export default NotePage;
