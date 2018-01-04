4945f3be1f64da704ffd40beb29bb17908aff609
CharzaroSmallLaptop <daball86@gmail.com>
2018-01-03 22:34:12 -0500

    test

--- initDB.js
+++ initDB.js
@@ -13,15 +13,14 @@ db.close();
 
 var pg = require('pg');
 var connectionURL = process.env.DATABASE_URL || 'postgres://localhost:5432/todo'
-console.log(process.env.DATABASE_URL);
-console.log(connectionURL)
 
 const client = new pg.Client(connectionURL);
 client.connect().then(() => {
 	console.log("HELLO\n\n\n\n");
-	//const query = client.query('CREATE TABLE users (username VARCHAR(252), hash VARCHAR(252), salt VARCHAR(252), key VARCHAR(252))', (err) => {
-	//	console.log("this works")
-	//});
+	const query = client.query('CREATE TABLE users (username VARCHAR(252), hash VARCHAR(252), salt VARCHAR(252), key VARCHAR(252))', (err) => {
+		console.log('error');
+	})
+	query.on('end', () => console.log('end'));
 })
 /*
 query.on('end', () => {
